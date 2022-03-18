4f51e5de40bc389a860605180cc1384a57a239eb
AigarsAbolins <abolins.aigars@gmail.com>
2022-03-18 11:30:16 +0200

    aaa

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-//console.log(1)
-
+//console.log(1)//
+let zina = document.querySelector('.manaZina');//let ir mainīgais.Comanda uzzina kas ir rakstits laukumā//
+let zinas = document.querySelector('.chataZinas');
 
 function sutitZinu()
 {
     console.log('sutitZinu() darbojas');
-    let zina = document.querySelector('.manaZina');//let ir mainīgais.
-    Comanda uzzina kas ir rakstits laukumā//
-    let zinas = document.querySelector('.chataZinas');
+    
     zinas.innerHTML = zinas.innerHTML + '<br/>'zina.value;
+}
+
+
+async function ieladetChataZinas()
+{
+    let datiNoServera = await fetch('chatazinas.txt')//Fetč komanda ļauj piekļūt kautkam//
+    let dati = await datiNoServera.text();
+    zinas.innerHTML = dati;
 }
