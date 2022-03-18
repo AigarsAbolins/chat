4b7025b3abd775ed893bfd373f61f9db8dd55c20
AigarsAbolins <abolins.aigars@gmail.com>
2022-03-18 11:14:20 +0200

    ssf

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log(1)
+//console.log(1)
+
+
+function sutitZinu()
+{
+    console.log('sutitZinu() darbojas');
+    let zina = document.querySelector('.manaZina');//let ir mainīgais.
+    Comanda uzzina kas ir rakstits laukumā//
+    let zinas = document.querySelector('.chataZinas');
+    zinas.innerHTML = zinas.innerHTML + '<br/>'zina.value;
+}
